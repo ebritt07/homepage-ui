0d022b96271704ec78744d831aefde49fabdf87f
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2023-06-25 01:16:30 -0500

    final touches

---
+++ static/projects.js
@@ -0,0 +1,13 @@
+const projects = [
+  {
+    title: "HTML site",
+    link: "https://github.com/ebritt07/homepage-ui",
+    color: "#337ab7",
+  },
+  {
+    title: "java spring app",
+    link: "https://github.com/ebritt07/homepage-service",
+    color: "#fcb103",
+  },
+  // { title: "BTC Trader 3", link: "https://github.com/ebritt07/homepage-ui", color: "#d18290" },
+];
